e3c4b9040da9bb2cc9d5703f96babf214c7b7c2f
Tyree Pearson <tyreepearson88@gmail.com>
2025-03-09 18:39:29 -0500

    created .replit in cursor

---
+++ replit.nix
@@ -0,0 +1,13 @@
+{ pkgs }: {
+  deps = [
+    pkgs.nodejs-19_x
+    pkgs.nodePackages.typescript-language-server
+    pkgs.yarn
+    pkgs.esbuild
+    pkgs.nodePackages.prettier
+    pkgs.nodePackages.eslint
+    pkgs.tailwindcss
+    pkgs.nodePackages.postcss
+    pkgs.nodePackages.autoprefixer
+  ];
+}
